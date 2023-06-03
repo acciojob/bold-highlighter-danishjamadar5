bb8bef4bdb3d17161c8a0d15f3909bd2fbae1fad
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-06-03 09:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
+  // Get all the <strong> elements in the paragraph
+  var boldElements = document.querySelectorAll("p strong");
 
-
+  // Change the color of each <strong> element to green
+  boldElements.forEach(function (element) {
+    element.style.color = "green";
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
+  // Get all the <strong> elements in the paragraph
+  var boldElements = document.querySelectorAll("p strong");
 
-    
+  // Revert the color of each <strong> element back to black
+  boldElements.forEach(function (element) {
+    element.style.color = "black";
+  });
 }
